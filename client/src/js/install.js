a011f6f0031fb1af6c343e28659fb187a7d0c6cc
Chandler Davis <echandlerdavis@gmail.com>
2022-08-17 13:22:18 -0700

    initial code, with some modifications

---
+++ client/src/js/install.js
@@ -0,0 +1,12 @@
+const butInstall = document.getElementById('buttonInstall');
+
+// Logic for installing the PWA
+//I think these are all going to be in activity 25, maybe even 23
+// TODO: Add an event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {});
+
+// TODO: Implement a click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {});
+
+// TODO: Add an handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {});
